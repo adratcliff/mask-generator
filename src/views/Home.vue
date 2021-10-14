150e3c5a60c7354a9d3991a400b65f2e2ee949b0
Alexander <alexanderratcliff99@gmail.com>
2021-10-14 21:03:33 +1000

    load image, add to canvas, get rgba data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img ref="pic" src="../assets/logo.png">
+    <canvas ref="canvas">Browser does not support HTML5 canvas</canvas>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      width: 0,
+      height: 0,
+      rgba: {},
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    const img = this.$refs.pic;
+    img.addEventListener('load', e => {
+      this.width = img.clientWidth;
+      this.height = img.clientHeight;
+
+      const c = this.$refs.canvas;
+      c.width = this.width; c.height =this. height;
+
+      const ctx = c.getContext('2d');
+      ctx.drawImage(img, 0, 0);
+
+      const imgData = ctx.getImageData(0, 0,this. width,this. height);
+      const getRgbaVals = (data, val) => data.filter((v, idx) => idx % 4 === val);
+
+      this.rgba = {
+        red: getRgbaVals(imgData.data, 0),
+        green: getRgbaVals(imgData.data, 1),
+        blue: getRgbaVals(imgData.data, 2),
+        alpha: getRgbaVals(imgData.data, 3),
+      };
+
+      // const g = (getRgbaVals2(imgData.data, 0))
+      // console.log(g)
+      // console.log(g.map(a => a.map(val => val ? 'o' : ' ').join('')).join('\n'))
+
+      // let str = '';
+      // for (let i = 0; i < (width * height); i++) {
+      //   if (i % 200 === 0) str += '\n';
+      //   str += (rgba.red[i] || rgba.green[i] || rgba.blue[i]) ? 'm' : ' ';
+      // }
+      // console.log(str)
+    });
+  },
 }
 </script>
